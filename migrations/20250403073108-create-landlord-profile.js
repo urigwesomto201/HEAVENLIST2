f6c805ef206f61abe669a7132425392c000365e0
digitz20 <alaekekaebuka200@gmail.com>
2025-04-08 14:09:06 +1245

    early

--- migrations/20250403073108-create-landlord-profile.js
+++ migrations/20250403073108-create-landlord-profile.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('landlordProfiles', {
+    await queryInterface.createTable('LandlordProfiles', {
       id: {
         allowNull: false,
         primaryKey: true,
@@ -50,6 +50,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('landlordProfiles');
+    await queryInterface.dropTable('LandlordProfiles');
   }
 };
